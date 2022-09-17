4c94a3aa8606d10d155849c3655208730103e884
Ali Ates <techxates@gmail.com>
2022-09-18 02:42:04 +0300

    username kısmını düzenledim

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar({openModal}) {
         <div className="flex justify-center items-center bg-white py-6 fixed w-full overflow-hidden">
             <h1 className="text-3xl tracking-[25px] text-b font-bold">TODO</h1>
            <div className="inset-y-4 right-8 absolute">
-            {getUserName() ?  `hoşgeldın ${getUserName()}` : 
+            {getUserName() ?  <p className="text-xl font-normal tracking-[2px] border border-b px-4 py-2 rounded-xl text-b">{`Hoşgeldin ${getUserName()}`}</p> : 
            <button onClick={openModal}
            className="bg-b px-4 py-2 rounded-xl text-white font-medium text-lg  ">
                 Sign in
